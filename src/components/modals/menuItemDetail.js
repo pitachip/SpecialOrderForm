df5c8dd913ebef04f2a058b043b438a69adc1283
Thrugreen Admin <admin@thrugreen.net>
2020-09-21 14:31:39 +0200

    modifier modal

--- src/components/modals/menuItemDetail.js
+++ src/components/modals/menuItemDetail.js
@@ -1,25 +1,63 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { connect } from "react-redux";
 
 class MenuItemDetail extends React.Component {
-	render() {
-		const { name } = this.props.menuItem[0];
+	renderModalStructure = (name, modifiers) => {
 		return (
-			<>
-				<Modal show={this.props.show} onHide={this.props.close}>
+			<div>
+				<Modal size="lg" show={this.props.show} onHide={this.props.close}>
 					<Modal.Header closeButton>
 						<Modal.Title>{name}</Modal.Title>
 					</Modal.Header>
-					<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+					<Modal.Body>
+						{modifiers ? this.renderModifierSections(modifiers) : null}
+					</Modal.Body>
 					<Modal.Footer>
 						<Button variant="secondary" onClick={this.props.close}>
 							Close
 						</Button>
 					</Modal.Footer>
 				</Modal>
-			</>
+			</div>
+		);
+	};
+	renderModifierSections = (modifiers) => {
+		return modifiers.map((item) => {
+			console.log(item);
+			return (
+				<div key={item.name}>
+					<h4 key={item.name}>{item.name}</h4>
+					<p>Choose up to {item.max_number_options}</p>
+					<Form>
+						<Row>{this.renderModifierOptions(item.options)}</Row>
+					</Form>
+				</div>
+			);
+		});
+	};
+
+	renderModifierOptions = (options) => {
+		return options.map((option) => {
+			return (
+				<Col xs={6}>
+					<Form.Check inline type="checkbox" label={option.name} />
+				</Col>
+			);
+		});
+	};
+	render() {
+		/**
+		 * TODO: have to figure out how to add individual validation to each form group
+		 */
+		const { name, modifiers } = this.props.menuItem[0];
+
+		return (
+			<div>{name ? this.renderModalStructure(name, modifiers) : null}</div>
 		);
 	}
 }
