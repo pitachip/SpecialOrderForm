3487ddbb58ed89d50869e82b6e4a1f9048031d02
Thrugreen Admin <alsaadirend@gmail.com>
2022-10-18 09:08:06 -0400

    Custom tips
    added custom tips

--- src/components/payment/payment-components/tip.js
+++ src/components/payment/payment-components/tip.js
@@ -1,12 +1,13 @@
 //libs
+import { toInteger } from "lodash";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 //ui components
-import { Button, Header } from "semantic-ui-react";
-//utils
-import { calculateTotals } from "../../../utils/orderCheckoutUtils";
+import { Button, Header, Input, Icon } from "semantic-ui-react";
 //actions
 import { updateOrderTotals } from "../../../actions";
+//css
+import "../payment-css/tip.css";
 
 const Tip = () => {
 	const orderTotals = useSelector((state) => state.order.totals);
@@ -17,6 +18,25 @@ const Tip = () => {
 		const tipAmount = orderTotals.subTotal * percentage;
 		let updatedTotal = orderTotals;
 		updatedTotal.tip = tipAmount;
+		updatedTotal = {
+			subTotal: updatedTotal.subTotal,
+			tax: updatedTotal.tax,
+			tip: updatedTotal.tip,
+			delivery: updatedTotal.delivery,
+			total: +(
+				updatedTotal.subTotal +
+				updatedTotal.tax +
+				updatedTotal.tip +
+				updatedTotal.delivery
+			).toFixed(2),
+		};
+		dispatch(updateOrderTotals(updatedTotal));
+	};
+
+	//TODO: make one function out of the tip calculation
+	const customTipAmountUpdated = (amount) => {
+		let updatedTotal = orderTotals;
+		updatedTotal.tip = amount;
 		updatedTotal = {
 			subTotal: updatedTotal.subTotal,
 			tax: updatedTotal.tax,
@@ -80,7 +100,21 @@ const Tip = () => {
 					No Tip
 				</Button>
 			</Button.Group>
-			<p>Custom Amount</p>
+			<div className="tipContainer">
+				<Header as="h5">Custom Amount</Header>
+				<Input
+					type="number"
+					iconPosition="left"
+					placeholder="Enter custom tip"
+					onChange={(event, data) => {
+						setActiveButton("0");
+						customTipAmountUpdated(parseFloat(data.value));
+					}}
+				>
+					<Icon name="dollar" />
+					<input />
+				</Input>
+			</div>
 		</>
 	);
 };
